30d27919bdecef9e9675d22aa11b7e8795bfe33c
Kingblu33 <demmarblue@gmail.com>
2024-03-08 16:32:11 -0500

    added errors

--- boxgenerator/src/components/BoxGen.js
+++ boxgenerator/src/components/BoxGen.js
@@ -1,75 +1,93 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
 import ShowBoxes from './ShowBoxes';
-
-
-
 
 const BoxGen = (props) => {
 
-    const [allColors, setAll_colors] = useState([]);
+    const [allColors, setAllColors] = useState([]);
     const [color, setColor] = useState("");
-    const [box_size, setBox_size] = useState(0);
+    const [boxSize, setBoxSize] = useState(0);
+    const [error, setError] = useState("");
+    const [colorError, setColorError] = useState("");
+    
+    const mainColors = [
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Orange",
+        "Purple",
+        "Pink",
+        "Brown",
+        "Black",
+        "White"
+    ];
 
-    //function to handle the state of the form input and update it
     const handleColor = (e) => {
         setColor(e.target.value);
-
-
+        setColorError(!mainColors.includes(e.target.value) ? "Enter a valid color" : "");
     }
 
     const handleBoxSize = e => {
-
-        setBox_size(e.target.value);
+        setBoxSize(e.target.value);
+        setError(e.target.value <= 200 ? "Please enter a value greater than 200 pixels" : "");
     }
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const newColor = {
-            color: color,
-            box_size: box_size
-        };
-        setAll_colors([...allColors, newColor]);
+        if (!error && !colorError) {
+            const newColor = {
+                color: color,
+                boxSize: boxSize
+            };
+            setAllColors([...allColors, newColor]);
 
-        // Ninja Bonus: clear out the color input on successful submission.
-        //to clear the input box after submission, we need to set the input box to an empty string
-        setColor('');
-        setBox_size(0);
-
+            // Clear out the input fields on successful submission
+            setColor('');
+            setBoxSize(0);
+        }
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
+                    <h1 className='error'>{error}</h1>
+                    <h1 className='error'>{colorError}</h1>
                     <label htmlFor="colorInput">Color</label>
-                    <input className='form-control'
-                        type="text" name="color" value={color}
+                    <input
+                        className='form-control'
+                        type="text"
+                        name="color"
+                        value={color}
                         onChange={handleColor}
                     />
-
                 </div>
 
                 <div>
                     <label htmlFor="boxSizeInput">Enter the box size in pixels</label>
                     <input
                         className='form-control'
-                        type="number" name="box_size" value={box_size}
+                        type="number"
+                        name="box_size"
+                        value={boxSize}
                         onChange={handleBoxSize}
-
-
                     />
                 </div>
-                <button className="btn btn-success active  form-control mt-3" type="submit" >Add</button>
 
+                <button
+                    className="btn btn-success active form-control mt-3"
+                    type="submit"
+                    disabled={error || colorError}
+                >
+                    Add
+                </button>
             </form>
+
             <h1 className="text-center">Colors</h1>
             <ShowBoxes className="align-items center" allColors={allColors} />
         </div>
     );
 }
-
 
 export default BoxGen;
